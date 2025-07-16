066790426add9088e97395647bde5355e4894498
Daksh Naithani <dakshnaithani2004@gmail.com>
2025-07-16 16:52:29 +0530

    test

--- regex/regex.py
+++ regex/regex.py
@@ -1,5 +1,5 @@
 import re
-string1='''essay paragraph is a distinct unit of writing, typically composed
+string1='''essay how tr paragraph is a distinct unit of writing, typically composed
            of multiple sentences, that focuses on a single idea or topic. 
            It serves as a building block for longer pieces of writing like 
            essays, helping to organize and structure information for better readability des'''
