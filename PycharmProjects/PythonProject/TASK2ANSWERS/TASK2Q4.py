0ae0851b597b8c01b0c5cd1e8f0512baa1fc61bf
Daksh-ino5 <dakshnaithani2004@gmail.com>
2025-07-16 17:36:55 +0530

    Delete PycharmProjects/PythonProject directory

--- PycharmProjects/PythonProject/TASK2ANSWERS/TASK2Q4.py
+++
@@ -1,8 +0,0 @@
-import pandas as pd
-
-employee_code_url='http://raw.githubusercontent.com/Daksh-ino5/Excitel-Internship/refs/heads/main/task%202/employee_codes.csv'
-df=pd.read_csv(employee_code_url)
-list_codes=['E101','E102','E103']#created a list as the filen was missing
-matched=df[df['Code'].isin(list_codes)]
-print("Doing the reverse lookup")
-print(matched[['Code','Name']])
