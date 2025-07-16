a84ce485b0d690261a19105bcc8229922bf8a606
Chirag Bhatnagar <100939388+chiragbhatn@users.noreply.github.com>
2025-07-16 18:28:57 +0530

    Create phonenumber.py

---
+++ task 3/phonenumber.py
@@ -0,0 +1,12 @@
+string = '''
++91 9876543210 -- junk@#$%data1@@@some_text_here
+HelloWorld +91-9123456789 :: random_text_123 ### ????
+Info: +91 9988776655 &*()_++}{>>“”abc_data_xyz
+Customer: +91 9090909090 | garbage--text!!!--junk-data
+user001: +91 7788990011 lorem$$$ipsum###donotuse
++91 8888888888 <<-->> text_before && after ~!@#
+Name: Rohan, Contact: +91 9812345678 ::!!!--misc-data--!!
+Note: +91 7000070000 / test_case__begin__ @#$_end
+Data+++: +91 9345678901 $$$random###string$$$data
++91 8008008008 -->*<&% messy_random__##text!!inserted
+'''
